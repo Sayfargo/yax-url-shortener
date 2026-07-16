af2ce080249baca0071005aa9697b68709e3b92c
Sayf4rgo <sayf4rgo@gmail.com>
2026-07-17 00:57:19 +0500

    add graceful shutdown with signal

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"os/signal"
+	"syscall"
+
+	core_app "github.com/Sayfargo/yax-url-shortener/internal/core/app"
+)
+
+func main() {
+
+	sigCtx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+
+	app := core_app.New()
+
+	if err := app.Run(sigCtx); err != nil {
+		fmt.Println(err)
+	}
+}
