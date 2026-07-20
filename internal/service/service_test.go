fc1ac38affe06d75aed728a1f00e8d8356564b47
Sayf4rgo <sayf4rgo@gmail.com>
2026-07-20 18:11:26 +0500

    add more test cases and add gonanoid interface

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -6,12 +6,18 @@ import (
 
 	"net/url"
 
+	"github.com/Sayfargo/yax-url-shortener/internal/repository"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
+// Мок для репозитория, чтобы создать экземпляр сервиса
 type MockURLRepository struct {
+	mock.Mock
+}
+
+type MockGoNanoIDGenerator struct {
 	mock.Mock
 }
 
@@ -25,6 +31,90 @@ func (m *MockURLRepository) Get(ctx context.Context, shortCode string) (string, 
 	return args.String(0), args.Error(1)
 }
 
+func (m *MockGoNanoIDGenerator) Generate(alphabet string, size int) (string, error) {
+	args := m.Called(alphabet, size)
+	return args.String(0), args.Error(1)
+}
+
+func TestGetOriginalUrl_UrlNotExists(t *testing.T) {
+	mockRepo := new(MockURLRepository)
+	mockRepo.On("Get", mock.Anything, mock.Anything).Return("", repository.ErrNotExists)
+
+	t.Cleanup(func() {
+		mockRepo.AssertExpectations(t)
+	})
+
+	svc := New(mockRepo, new(GoNanoIDGenerator))
+
+	result, err := svc.GetOriginalUrl(context.Background(), "fKM29FzE")
+	assert.Empty(t, result)
+	assert.ErrorIs(t, err, ErrUrlDoesNotExists)
+}
+
+func TestCreateShortUrl_ConflictRetry(t *testing.T) {
+
+	mockRepo := new(MockURLRepository)
+	mockGenerator := new(MockGoNanoIDGenerator)
+
+	mockGenerator.On("Generate", alphabet, size).Return("ZEFIRMOY", nil).Once()
+	mockGenerator.On("Generate", alphabet, size).Return("BULOCHKA", nil).Once()
+
+	mockRepo.On("Create", mock.Anything, "random.com", "ZEFIRMOY").Return(repository.ErrAlreadyExists).Once()
+	mockRepo.On("Create", mock.Anything, "random.com", "BULOCHKA").Return(nil).Once()
+
+	t.Cleanup(func() {
+		mockGenerator.AssertExpectations(t)
+		mockRepo.AssertExpectations(t)
+	})
+
+	svc := New(mockRepo, mockGenerator)
+
+	expectedUrl := svc.buildShortedUrl("BULOCHKA")
+	require.NotEmpty(t, expectedUrl)
+
+	result, err := svc.CreateShortUrl(context.Background(), "random.com")
+
+	require.NoError(t, err)
+	assert.Equal(t, expectedUrl, result)
+}
+
+func TestGetOriginalUrl_ContextCanceled(t *testing.T) {
+	// Params
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	mockRepo := new(MockURLRepository)
+	mockRepo.On("Get", mock.Anything, mock.Anything).Return("", nil)
+
+	t.Cleanup(func() {
+		mockRepo.AssertNotCalled(t, "Get")
+	})
+
+	svc := New(mockRepo, new(GoNanoIDGenerator))
+
+	result, err := svc.GetOriginalUrl(ctx, "shortCode")
+	assert.Empty(t, result)
+	assert.ErrorIs(t, err, context.Canceled)
+}
+func TestCreateShortUrl_ContextCanceled(t *testing.T) {
+	// Params
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	mockRepo := new(MockURLRepository)
+	mockRepo.On("Create", mock.Anything, mock.Anything).Return(nil)
+
+	t.Cleanup(func() {
+		mockRepo.AssertNotCalled(t, "Create")
+	})
+
+	svc := New(mockRepo, new(GoNanoIDGenerator))
+
+	result, err := svc.CreateShortUrl(ctx, "anything")
+	assert.Empty(t, result)
+	assert.ErrorIs(t, err, context.Canceled)
+}
+
 func TestGetOriginalUrl_HappyCase(t *testing.T) {
 	// Params
 	expectedUrl := "https://google.com"
@@ -36,7 +126,7 @@ func TestGetOriginalUrl_HappyCase(t *testing.T) {
 		mockRep.AssertExpectations(t)
 	})
 
-	svc := New(mockRep)
+	svc := New(mockRep, new(GoNanoIDGenerator))
 
 	originalUrl, err := svc.GetOriginalUrl(context.Background(), "FLeq19fl")
 	require.NoError(t, err)
@@ -57,7 +147,7 @@ func TestCreateShortUrl_HappyCase(t *testing.T) {
 		mockRep.AssertExpectations(t)
 	})
 
-	svc := New(mockRep)
+	svc := New(mockRep, new(GoNanoIDGenerator))
 
 	shortedUrl, err := svc.CreateShortUrl(context.Background(), testUrl)
 	require.NoError(t, err)
