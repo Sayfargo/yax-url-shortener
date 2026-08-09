4c664193d911a33ff6afaa14601c245b479d1ee4
Sayf4rgo <sayf4rgo@gmail.com>
2026-08-09 20:01:46 +0500

    refactor - rewrite logger with new architecture

--- internal/core/slogger/slogger.go
+++ internal/core/slogger/slogger.go
@@ -6,135 +6,112 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
-	"strings"
+	"sync"
 	"time"
+
+	config_slogger "github.com/Sayfargo/yax-url-shortener/internal/config/slogger"
 )
 
 type Slogger struct {
 	*slog.Logger
-
-	files logFiles
 }
 
-type logFiles struct {
-	appFile   *os.File
-	errorFile *os.File
+type Closer struct {
+	files []*os.File
+	once  sync.Once
+	errs  error
 }
 
 var (
 	ErrNoLevelOrDirectory = errors.New("missing directory or level")
 )
 
-func New(dir, levelStdout, levelFile string) (*Slogger, error) {
+func New(cfg config_slogger.Config) (*Slogger, *Closer, error) {
 
-	// Если ничего не передано, то возвращаем ошибку
-	if levelStdout == "" || dir == "" || levelFile == "" {
-		return nil, ErrNoLevelOrDirectory
-	}
+	handler, closer, err := buildHandler(cfg)
 
-	var slogLevelStdout, slogLevelAppFile slog.Level
-	// Преобразуем level в верхний регистр и парсим его в slog.Level
-	if err := slogLevelStdout.UnmarshalText([]byte(strings.ToUpper(levelStdout))); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal stdout level: %w", err)
-	}
-
-	if err := slogLevelAppFile.UnmarshalText([]byte(strings.ToUpper(levelFile))); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal app log file level: %w", err)
-	}
-
-	// Создаем handler для вывода в stdout с заданным уровнем логирования
-	stdoutHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slogLevelStdout,
-	})
-
-	logFiles, err := makeLogFiles(dir, levelFile)
 	if err != nil {
-		return nil, fmt.Errorf("failed to make log file: %w", err)
+		return nil, nil, err
 	}
-
-	// Обработчик для вывода в app.log (все детальные события)
-	appFileHandler := slog.NewTextHandler(logFiles.appFile, &slog.HandlerOptions{
-		Level: slogLevelAppFile,
-	})
-	// Обработчик для вывода в error.log (ошибки)
-	errorFileHandler := slog.NewTextHandler(logFiles.errorFile, &slog.HandlerOptions{
-		Level: slog.LevelError,
-	})
-
-	multiHandler := slog.NewMultiHandler(stdoutHandler, appFileHandler, errorFileHandler)
 
 	return &Slogger{
-		Logger: slog.New(multiHandler),
-		files:  logFiles,
-	}, nil
-}
-
-func makeLogFiles(dir, level string) (logFiles, error) {
-
-	// Создаем директорию для логов, если она не существует
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return logFiles{}, fmt.Errorf("failed to make log dir: %w", err)
-	}
-	// Задаём формат времени для имени файла
-	timestamp := time.Now().UTC().Format("2006-01-02T15-04-05.000000")
-
-	appLogFilePath := filepath.Join(dir, fmt.Sprintf("%s-%s-app.log", timestamp, level))
-
-	errorLogFilePath := filepath.Join(dir, fmt.Sprintf("%s-%s-error.log", timestamp, "ERROR"))
-
-	// После создания файла, открываем его для записи
-	applogFile, err := os.OpenFile(appLogFilePath, os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return logFiles{}, fmt.Errorf("failed to open app log file: %w", err)
-	}
-
-	errorLogFile, err := os.OpenFile(errorLogFilePath, os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		applogFile.Close()
-		return logFiles{}, fmt.Errorf("failed to open error log file: %w", err)
-	}
-
-	return logFiles{
-		appFile:   applogFile,
-		errorFile: errorLogFile,
-	}, nil
+		Logger: slog.New(handler),
+	}, closer, err
 }
 
 // MustNew либо создаст экземпляр либо вызовет panic в случае ошибки
-func MustNew(dir, levelStdout, levelFile string) *Slogger {
-	slogger, err := New(dir, levelStdout, levelFile)
+func MustNew(cfg config_slogger.Config) (*Slogger, *Closer) {
+	slogger, closer, err := New(cfg)
 	if err != nil {
 		panic(err)
 	}
-	return slogger
+	return slogger, closer
 }
 
-// Обёртка для функции slog.Logger.With
-func (l *Slogger) With(args ...any) *Slogger {
-	return &Slogger{
-		Logger: l.Logger.With(args...),
-		files:  l.files,
+func buildHandler(cfg config_slogger.Config) (handler slog.Handler, closer *Closer, err error) {
+
+	handlers := make([]slog.Handler, 0)
+
+	files := make([]*os.File, 0)
+
+	defer func() {
+		if err != nil {
+			for _, file := range files {
+				file.Close()
+			}
+		}
+	}()
+
+	if cfg.Stdout.Enabled {
+		handlers = append(handlers, newStdoutHandler(cfg.Stdout))
 	}
-}
 
-func (l *Slogger) Close() error {
-	var errs error
+	for _, fileConfig := range cfg.Files {
+		if fileConfig.Enabled {
 
-	if l.files.appFile != nil {
-		if err := l.files.appFile.Close(); err != nil {
-			errs = errors.Join(errs, fmt.Errorf("failed to close %s log file: %w", l.files.appFile.Name(), err))
+			file, err := initLogFile(cfg.Directory, fileConfig.Name, fileConfig.Level.String())
+			if err != nil {
+				return nil, nil, err
+			}
+			files = append(files, file)
+			handlers = append(handlers, newFileHandler(fileConfig, file))
 		}
 	}
 
-	if l.files.errorFile != nil {
-		if err := l.files.errorFile.Close(); err != nil {
-			errs = errors.Join(errs, fmt.Errorf("failed to close %s log file: %w", l.files.errorFile.Name(), err))
+	handler = slog.NewMultiHandler(handlers...)
+
+	return handler, &Closer{
+		files: files,
+	}, nil
+
+}
+
+func initLogFile(dir, name, level string) (*os.File, error) {
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return nil, fmt.Errorf("failed to make log dir: %w", err)
+	}
+	timestamp := time.Now().UTC().Format("2006-01-02T15-04-05.000000")
+
+	logFilePath := filepath.Join(dir, fmt.Sprintf("%s-%s-%s.log", timestamp, level, name))
+
+	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open app log file: %w", err)
+	}
+
+	return logFile, nil
+}
+
+func (c *Closer) Close() error {
+
+	c.once.Do(func() {
+		for _, file := range c.files {
+			if err := file.Close(); err != nil {
+				c.errs = errors.Join(c.errs, fmt.Errorf("Failed to close file with name %s: %w", file.Name(), err))
+			}
 		}
-	}
+	})
 
-	if errs != nil {
-		return errs
-	}
-
-	return nil
+	return c.errs
 }
