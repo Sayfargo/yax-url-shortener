4de4fe1e26e8d857b84169833af50ebe0ffcca2c
Sayf4rgo <sayf4rgo@gmail.com>
2026-08-14 20:58:27 +0500

    relocate config and logger setup to main

--- internal/core/app/app.go
+++ internal/core/app/app.go
@@ -2,10 +2,10 @@ package core_app
 
 import (
 	"context"
-	"os"
+	"errors"
+	"fmt"
 
 	"github.com/Sayfargo/yax-url-shortener/internal/config"
-	config_slogger "github.com/Sayfargo/yax-url-shortener/internal/config/slogger"
 	core_server "github.com/Sayfargo/yax-url-shortener/internal/core/server"
 	core_slogger "github.com/Sayfargo/yax-url-shortener/internal/core/slogger"
 	core_storage_cache "github.com/Sayfargo/yax-url-shortener/internal/core/storage/cache"
@@ -13,65 +13,97 @@ import (
 	core_transport_http_middleware "github.com/Sayfargo/yax-url-shortener/internal/core/transport/http/middleware"
 	"github.com/Sayfargo/yax-url-shortener/internal/handler"
 	repository_cache "github.com/Sayfargo/yax-url-shortener/internal/repository/cache"
-	repository_file "github.com/Sayfargo/yax-url-shortener/internal/repository/file"
 	"github.com/Sayfargo/yax-url-shortener/internal/service"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator/v10"
 )
 
 type App struct {
-	// HTTP server
 	Server  *core_server.HTTPServer
 	Storage *core_storage_file.FileStorage
 }
 
-func New() *App {
+func New(cfg *config.Config, slog *core_slogger.Slogger) (*App, error) {
 
-	cfg := config.Load(os.Args[1:])
+	// chi router/middlewares
 	rootRouter := chi.NewRouter()
-	// Пока slogger временно без env
-	slog, _ := core_slogger.MustNew(config_slogger.Config{
-		Directory: "./log",
-		Stdout:    config_slogger.StdoutConfig{Enabled: true, Format: config_slogger.FormatText, Writer: os.Stdout},
-	})
-
 	rootRouter.Use(core_transport_http_middleware.Logging(slog))
 	rootRouter.Use(core_transport_http_middleware.GzipCompress())
-	validate := validator.New()
 
+	// storages
 	cacheStorage := core_storage_cache.Init()
-	// Подумать как и где закрывать storage. Может быть написать closer для di
-	fileStorage, err := core_storage_file.New(cfg.FileStorage)
-
+	fileStorage, err := core_storage_file.Init(cfg.FileStorage)
 	if err != nil {
+
 		slog.Error(
-			"Failed to init file storage",
-			"error:", err,
+			"failed to initialize file storage",
+			"err", err,
 		)
+
+		return nil, fmt.Errorf("file storage init: %w", err)
 	}
 
-	cacheRepo := repository_cache.New(cacheStorage)
-	fileRepo := repository_file.New(fileStorage)
+	// repositories
+	cr := repository_cache.New(cacheStorage)
+	fcr := repository_cache.Wrap(cr, fileStorage)
 
-	svc := service.New(cacheRepo, fileRepo, new(service.GoNanoIDGenerator), cfg.Server.BaseURL, validate)
-	handler := handler.New(svc)
+	slog.Info(
+		"starting restore data into cache",
+	)
+
+	// restore
+	if err := fcr.Restore(); err != nil {
+
+		slog.Error(
+			"failed to restore data into cache",
+			"err", err,
+		)
+
+		_ = fileStorage.Close()
+
+		return nil, fmt.Errorf("restore cache: %w", err)
+	}
+
+	slog.Info(
+		"data restoring succefully done",
+	)
+
+	svc := service.New(fcr, new(service.GoNanoIDGenerator), cfg.Server.BaseURL, validator.New(), slog)
+	handler := handler.New(svc, slog)
 	handler.Register(rootRouter)
 
-	httpServer := core_server.New(rootRouter, cfg.Server)
+	httpServer := core_server.New(rootRouter, cfg.Server, slog)
 
 	return &App{
 		Server:  httpServer,
 		Storage: fileStorage,
-	}
+	}, nil
 
 }
 
-func (a *App) Run(ctx context.Context) error {
+func (a *App) Run(ctx context.Context) (errs error) {
 
-	defer a.Storage.Close()
+	defer func() {
+
+		if a.Storage == nil {
+			return
+		}
+
+		if err := a.Storage.Close(); err != nil {
+			errs = errors.Join(
+				errs,
+				fmt.Errorf("file storage close: %w", err),
+			)
+		}
+	}()
 
 	if err := a.Server.Run(ctx); err != nil {
-		return err
+		errs = errors.Join(errs, fmt.Errorf("server run: %w", err))
 	}
+
+	if errs != nil {
+		return errs
+	}
+
 	return nil
 }
