77c612e4e49207c708d7d71c735c2ae4e3a21b3a
Sayf4rgo <sayf4rgo@gmail.com>
2026-08-19 17:14:00 +0500

    chore: allow app to start without db for autotests compatibility

--- internal/core/db/postgres/postgres.go
+++ internal/core/db/postgres/postgres.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
-	"time"
 
 	config_db_postgres "github.com/Sayfargo/yax-url-shortener/internal/config/db/postgres"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -37,17 +36,17 @@ func New(cfg config_db_postgres.Config, log *slog.Logger) (*pgxpool.Pool, error)
 		return nil, fmt.Errorf("new with config: %w", err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// defer cancel()
 
-	if err := pool.Ping(ctx); err != nil {
-		log.Error(
-			"failed to ping database",
-			"err", err,
-		)
-		pool.Close()
-		return nil, fmt.Errorf("ping database: %w", err)
-	}
+	// if err := pool.Ping(ctx); err != nil {
+	// 	log.Error(
+	// 		"failed to ping database",
+	// 		"err", err,
+	// 	)
+	// 	pool.Close()
+	// 	return nil, fmt.Errorf("ping database: %w", err)
+	// }
 
 	log.Info(
 		"database initialized successfully",
