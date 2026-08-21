c1650a200e89a6387e492e7fbf04a342b5b0166d
Sayf4rgo <sayf4rgo@gmail.com>
2026-08-21 21:12:41 +0500

    feat: add custom batch conflict error

--- internal/repository/errors/errorrs.go
+++ internal/repository/errors/errorrs.go
@@ -1,9 +1,21 @@
 package repository_errors
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	ErrNotExists      = errors.New("row not found in db")
 	ErrAlreadyExists  = errors.New("row already exists")
 	ErrUnexpectedType = errors.New("unexpected data type in cache")
 )
+
+type BatchConflictError struct {
+	Index int
+	Err   error
+}
+
+func (e *BatchConflictError) Error() string {
+	return fmt.Sprintf("conflict at index %d: %v", e.Index, e.Err)
+}
