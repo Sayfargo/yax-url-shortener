41760133ef16441de7fb26e0bfce46443751f375
Sayf4rgo <sayf4rgo@gmail.com>
2026-08-21 21:11:29 +0500

    feat: add batch URL shortening endpoint

--- internal/handler/dto.go
+++ internal/handler/dto.go
@@ -7,3 +7,13 @@ type ShortUrlRequest struct {
 type ShortUrlResponse struct {
 	Result string `json:"result"`
 }
+
+type CreateUrlBatchRequest struct {
+	CorrelationID string `json:"correlation_id"`
+	OriginalURL   string `json:"original_url"`
+}
+
+type CreateUrlBatchResponse struct {
+	CorrelationID string `json:"correlation_id"`
+	ShortURL      string `json:"short_url"`
+}
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -14,6 +14,7 @@ import (
 
 type UrlShortener interface {
 	CreateShortUrl(ctx context.Context, url string) (string, error)
+	CreateUrlBatch(ctx context.Context, req []service.CreateUrlBatchRequest) ([]service.CreateUrlBatchResponse, error)
 	GetOriginalUrl(ctx context.Context, shortCode string) (string, error)
 }
 
@@ -27,6 +28,8 @@ type DBHealthChecker interface {
 	Ping(ctx context.Context) error
 }
 
+const maxBodySize = 1024 * 1024
+
 func New(service UrlShortener, log *slog.Logger, db DBHealthChecker) *Handler {
 	return &Handler{
 		service: service,
@@ -38,8 +41,99 @@ func New(service UrlShortener, log *slog.Logger, db DBHealthChecker) *Handler {
 func (h *Handler) Register(r chi.Router) {
 	r.Post("/", h.Create)
 	r.Post("/api/shorten", h.Shorten)
+	r.Post("/api/shorten/batch", h.ShortenBatch)
 	r.Get("/{id}", h.Redirect)
 	r.Get("/ping", h.Ping)
+}
+
+func (h *Handler) ShortenBatch(w http.ResponseWriter, r *http.Request) {
+
+	var request []CreateUrlBatchRequest
+
+	body := http.MaxBytesReader(
+		w,
+		r.Body,
+		maxBodySize,
+	)
+
+	if err := json.NewDecoder(body).Decode(&request); err != nil {
+
+		var maxBytesErr *http.MaxBytesError
+		if errors.As(err, &maxBytesErr) {
+
+			h.log.Warn("request body exceeded max allowed size",
+				slog.Int64("limit_bytes", maxBytesErr.Limit),
+				slog.String("path", r.URL.Path),
+				slog.String("remote_addr", r.RemoteAddr),
+			)
+
+			w.WriteHeader(http.StatusRequestEntityTooLarge)
+		} else {
+			h.log.Info(
+				"failed to decode json body",
+				"err", err,
+			)
+
+			http.Error(w, "failed to read request body", http.StatusBadRequest)
+		}
+		return
+	}
+
+	serviceReq := make([]service.CreateUrlBatchRequest, len(request))
+
+	for i, item := range request {
+		serviceReq[i] = service.CreateUrlBatchRequest{
+			CorrelationID: item.CorrelationID,
+			OriginalURL:   item.OriginalURL,
+		}
+	}
+
+	resp, err := h.service.CreateUrlBatch(r.Context(), serviceReq)
+	if err != nil {
+		if errors.Is(err, context.Canceled) {
+			h.log.Debug("create short url canceled by client")
+			w.WriteHeader(499)
+		} else if errors.Is(err, service.ErrIncorrectUrl) {
+
+			h.log.Info(
+				"incorrect url request",
+				"err", err,
+			)
+			// ошибка содержит информацию на какой именно строке и с каким id возникла ошибка для удобства
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		} else if errors.Is(err, service.ErrShortCodeCollisionLimitExceeded) {
+			h.log.Warn(
+				"short code collision limit exceeded",
+			)
+			http.Error(w, "failed to process request, please try again", http.StatusInternalServerError)
+		} else if errors.Is(err, service.ErrEmptyBatch) {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+
+		} else {
+			h.log.Error(
+				"unexpected error during url shortening",
+				"err", err,
+			)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	data, err := json.Marshal(resp)
+	if err != nil {
+		h.log.Error(
+			"failed to marshal response json",
+			"err", err,
+		)
+
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(data)
+
 }
 
 func (h *Handler) Ping(w http.ResponseWriter, r *http.Request) {
@@ -104,8 +198,13 @@ func (h *Handler) Shorten(w http.ResponseWriter, r *http.Request) {
 		response ShortUrlResponse
 	)
 
-	if err := json.NewDecoder(io.LimitReader(r.Body, 1024*1024)).Decode(&request); err != nil {
+	body := http.MaxBytesReader(
+		w,
+		r.Body,
+		maxBodySize,
+	)
 
+	if err := json.NewDecoder(body).Decode(&request); err != nil {
 		h.log.Info(
 			"failed to decode json body",
 			"err", err,
@@ -132,6 +231,7 @@ func (h *Handler) Shorten(w http.ResponseWriter, r *http.Request) {
 
 			h.log.Info(
 				"incorrect url request",
+				"err", err,
 				"url", request.URL,
 			)
 
@@ -151,9 +251,8 @@ func (h *Handler) Shorten(w http.ResponseWriter, r *http.Request) {
 
 	response.Result = shortedUrl
 
-	body, err := json.Marshal(response)
+	data, err := json.Marshal(response)
 	if err != nil {
-
 		h.log.Error(
 			"failed to marshal response json",
 			"err", err,
@@ -164,7 +263,7 @@ func (h *Handler) Shorten(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	w.Write(body)
+	w.Write(data)
 }
 
 func (h *Handler) Create(w http.ResponseWriter, r *http.Request) {
